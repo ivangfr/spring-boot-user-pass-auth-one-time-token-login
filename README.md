d22f57ea827a66d76b755949bbe9fab6ce46e10a
Ivan Franchin <ivan.franchin@takeaway.com>
2024-12-03 18:56:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-one-time-token-userpass-auth
---
+++ movies-app/src/main/java/com/ivanfranchin/moviesapp/MoviesAppApplication.java
@@ -0,0 +1,12 @@
+package com.ivanfranchin.moviesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesAppApplication.class, args);
+    }
+}
