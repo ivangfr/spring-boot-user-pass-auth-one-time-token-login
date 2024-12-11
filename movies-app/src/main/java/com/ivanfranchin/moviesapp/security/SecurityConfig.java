d4bf7b513180e721e64ad15085ec2838660c83db
Ivan Franchin <ivan.franchin@takeaway.com>
2024-12-11 06:41:09 -0300

    Remove unnecessary annotation in SecurityConfig

--- movies-app/src/main/java/com/ivanfranchin/moviesapp/security/SecurityConfig.java
+++ movies-app/src/main/java/com/ivanfranchin/moviesapp/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ivanfranchin.moviesapp.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,7 +9,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
